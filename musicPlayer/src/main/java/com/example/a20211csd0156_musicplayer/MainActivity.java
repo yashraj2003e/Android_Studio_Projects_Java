53b3c6b643fcca08f03b22795a115e893e8758a0
Yashraj <yashsharmau9@gmail.com>
2024-01-16 23:48:13 +0530

    Modified Music Player

---
+++ musicPlayer/src/main/java/com/example/a20211csd0156_musicplayer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a20211csd0156_musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
